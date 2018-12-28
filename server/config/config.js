d24593b173746e1900f4a34b83d129efa83fa75b
gustavoRuiz <jimgustavo1987@hotmail.com>
2018-12-28 11:54:07 -0500

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,6 @@
+// Database URL
+module.exports = {
+	// Uncomment to connect with MongoDB on Cloud
+	url: "mongodb://mongoVideo:mongoVideo87@ds119343.mlab.com:19343/mongovideo"
+	//'url': "mongodb://localhost/mvc-app-multimedia"
+};
